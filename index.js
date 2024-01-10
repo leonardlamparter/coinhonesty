594f95d641cccdb5b74b723c88a43a0b2ce459e4
Movies <leonardlamparter@gmail.com>
2024-01-10 16:22:43 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+function search(coin_name) {
+var coin = coins[coin_name] || {"name": "COIN NOT YET SCANNED","contract": "","defi": 0,"l_whale": 0,"funny": "","h24_vol": "$0.00","swaps": "0","ttl_liq": "$0.00","mc": 0,"bar_dta": [0, 0, 0],"lnks" : ["TBF","TBF","TBF","TBF","TBF","TBF"]}
+
+var dta = [coin.name, coin.contract, coin.defi, coin.l_whale, coin.funny, coin.h24_vol, coin.swaps, coin.ttl_liq, coin.mc]
+document.querySelectorAll("#info span").forEach((v, i) => {
+    v.innerHTML = dta[i]
+})
+
+var allhidden = true
+document.querySelectorAll("#lnks a").forEach((v, i) => {
+    coin.lnks[i] ? v.href = coin.lnks[i] : v.hidden = "hidden"
+    coin.lnks[i] ? allhidden = false : allhidden = allhidden
+})
+
+coin["lnks_e"] ? '' : document.querySelector("#lnksnotes").innerHTML = "No links nor socials found"
+
+var tags = ["Volume (millions)", "Swaps (ten thousands)", "Liquidity (millions)"]
+var res = coin.bar_dta // to be imported
+var barColors = [res[0]>=1, res[1]>=1, res[2]>=0.015]
+barColors = barColors.map(v => v? 'green':'red')
+
+new Chart("basicdata", {
+    type: "bar",
+    data: {
+      labels: tags,
+      datasets: [{
+        backgroundColor: barColors,
+        data: res
+      }]
+    },
+    options: {
+        legend: {display: false},
+        title: {
+          display: false
+        }
+    }
+});
+
+
+new Chart("holders", {
+    type: "pie",
+    data: {
+      labels: ["Largest Whale", "Other"],
+      datasets: [{
+        backgroundColor: ["blue", "white"],
+        data: [coin.l_whale, 100-coin.l_whale]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: "Holders"
+      }
+    }
+});
+}
+
+search(location.search.replace("?", "").toLocaleUpperCase())
---
+++ scans.js
@@ -0,0 +1,24 @@
+var coins = {
+    "USEDCAR":
+    {
+    "name": "USEDCAR",
+    "contract": "0x2f8c094453146dd154e3939f816186d5d6a458f3",
+    "defi": 30,
+    "l_whale": 6.896,
+    "funny": "😄",
+    "h24_vol": "$28,980,000.00",
+    "swaps": "133,880",
+    "ttl_liq": "$1,140,000.00",
+    "mc": 83,
+    "bar_dta": [28.980000, 13.3880, 114.000000],
+    "lnks" : [
+        false, // X
+        false, // De.Fi
+        false, // Etherscan
+        false, // Bubblemaps
+        false, // CMC
+        false  // CoinGecko
+    ],
+    "lnks_e" : false
+    }
+}
