286ea181de52f1e0d57e22246242dfc118bff1bd
Movies <leonardlamparter@gmail.com>
2024-01-10 17:05:39 -0400

    SNEK, DED. New Total: 3

--- scans.js
+++ scans.js
@@ -10,7 +10,7 @@ var coins = {
     "swaps": "133,880",
     "ttl_liq": "$1,140,000.00",
     "mc": 83,
-    "bar_dta": [28.980000, 13.3880, 114.000000],
+    "bar_dta": [28.98, 13.388, 1.14],
     "lnks" : [
         false, // X
         false, // De.Fi
@@ -20,5 +20,49 @@ var coins = {
         false  // CoinGecko
     ],
     "lnks_e" : false
-    }
+    },
+    "SNEK":
+    {
+    "name": "SNEK",
+    "contract": "0xe99230a033332ff10ff79e137d3d27a4417d014f",
+    "defi": 89,
+    "l_whale": 2.5,
+    "funny": "😆",
+    "h24_vol": "$4,329,370.00",
+    "swaps": "1,238,674",
+    "ttl_liq": "$1,170,000.00",
+    "mc": 83,
+    "bar_dta": [4.32937, 123.8674, 1.17],
+    "lnks" : [
+        "https://twitter.com/snekcoinada?lang=en", // X
+        "https://de.fi/scanner/contract/0xe99230a033332ff10ff79e137d3d27a4417d014f?chainId=eth", // De.Fi
+        "https://cardanoscan.io/token/279c909f348e533da5808898f87f9a14bb2c3dfbbacccd631d927a3f534e454b", // Cardanoscan
+        "https://app.bubblemaps.io/eth/token/0xe99230a033332ff10ff79e137d3d27a4417d014f", // Bubblemaps
+        "https://coinmarketcap.com/currencies/snek/", // CMC
+        "https://www.coingecko.com/en/coins/snek"  // CoinGecko
+    ],
+    "lnks_e" : true
+    },
+    "DED":
+    {
+    "name": "DED",
+    "contract": "Not launched yet",
+    "defi": 0,
+    "l_whale": 0,
+    "funny": "🤣",
+    "h24_vol": "$0.00",
+    "swaps": "0",
+    "ttl_liq": "$0.00",
+    "mc": 16,
+    "bar_dta": [0, 0, 0],
+    "lnks" : [
+        "https://twitter.com/ded_coin", // X
+        false, // De.Fi
+        false, // Cardanoscan
+        false, // Bubblemaps
+        false, // CMC
+        false  // CoinGecko
+    ],
+    "lnks_e" : true
+    },
 }
